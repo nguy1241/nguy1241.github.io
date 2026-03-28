74eb97f6ec3e4991f4d3580fe11559dc001a8c63
nguy1241 <nguy1241@msu.edu>
2026-03-28 18:47:41 -0500

    First update to website in a while, unsure what changes were made

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100, // Check for changes every 100ms (tweak if needed)
+    },
+  },
 });
